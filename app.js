15977a8d3104870d452a3806e362bf776962f477
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-12-02 23:45:38 +0200

    set up swagger ui and set swagger file

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // const config = require("config");
-const dotenv = require("dotenv");
-require("dotenv").config({ path: __dirname + "/.env" });
-const express = require("express");
-const cors = require("cors");
-const matchRouter = require("./matches/match.routes");
-const errorHandler = require("./middleware/errorhandler");
+const dotenv = require('dotenv');
+require('dotenv').config({ path: __dirname + '/.env' });
+const express = require('express');
+const cors = require('cors');
+const matchRouter = require('./matches/match.routes');
+const errorHandler = require('./middleware/errorhandler');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./offside-swagger-api.json');
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use("/matches", matchRouter);
+app.use(cors(), express.json());
+app.use('/', swaggerUi.serve);
+app.get('/', swaggerUi.setup(swaggerDocument));
+
+app.use('/matches', matchRouter);
 
 app.use(errorHandler);
 
 app.use((req, res) => {
-  res.status(404).json({ success: false, error: "Not found" });
+  res.status(404).json({ success: false, error: 'Not found' });
 });
 
 module.exports = app;
